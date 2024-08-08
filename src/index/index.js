85e5be3c5dab502949229b974c2f7659802a0460
Washington <washington.contato1327@gmail.com>
2024-08-08 14:01:21 -0300

    corrijindo alguns erros de renderização

--- src/index/index.js
+++ src/index/index.js
@@ -1,12 +1,16 @@
-import Home from "@/sessions/home/Home";
+import dynamic from "next/dynamic";
 import Styles from "./index.module.css";
 import Header from "@/sessions/header/Header";
 import Products from "@/sessions/products/products";
-import Clients from "@/sessions/ Satisfied/Satisfied";
-import Location from "@/sessions/Location/Location";
-import Contacts from "@/sessions/Contacts/Contacts";
 import About from "@/sessions/About/about";
 import Footer from "@/sessions/Footer/Footer";
+import Clients from "@/sessions/ Satisfied/Satisfied";
+import Home from "@/sessions/home/Home";
+import Contacts from "@/sessions/Contacts/Contacts";
+
+const Location = dynamic(() => import("@/sessions/Location/Location"), {
+  ssr: false,
+});
 
 export default function Index() {
   return (

06d393dec7c7d313a194d49ebe6e128746dd17c1
Washington <washington.contato1327@gmail.com>
2024-08-08 13:48:40 -0300

    Ajuste de erro client-server

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,3 +1,5 @@
+// components/Map.js
+
 "use client";
 
 import { useEffect } from "react";
@@ -7,20 +9,23 @@ import Styles from "./map.module.css";
 
 export default function Map() {
   useEffect(() => {
-    const map = L.map("map").setView([-23.55052, -46.633308], 13);
+    // Verifica se o código está sendo executado no lado do cliente
+    if (typeof window !== "undefined") {
+      const map = L.map("map").setView([-23.55052, -46.633308], 13);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "&copy; OpenStreetMap contributors",
-    }).addTo(map);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+      }).addTo(map);
 
-    L.marker([-23.55052, -46.633308])
-      .addTo(map)
-      .bindPopup("Beer Masters")
-      .openPopup();
+      L.marker([-23.55052, -46.633308])
+        .addTo(map)
+        .bindPopup("Beer Masters")
+        .openPopup();
 
-    return () => {
-      map.remove();
-    };
+      return () => {
+        map.remove();
+      };
+    }
   }, []);
 
   return (

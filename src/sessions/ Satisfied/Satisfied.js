63c02f3d921b01a0d72f40efb010e8795429231c
Washington <washington.contato1327@gmail.com>
2024-08-07 21:03:38 -0300

    Ajustando textos da sessao de clientes

--- src/sessions/ Satisfied/Satisfied.js
+++ src/sessions/ Satisfied/Satisfied.js
@@ -11,40 +11,48 @@ export default function Clients() {
           <div className={Styles.img}>
             <img src="/avatar1.svg" />
           </div>
-          <p>
-            A melhor IPA que já provei! Recomendo a todos os amantes de cerveja,
-            sensacional!
-          </p>
+          <div className={Styles.description}>
+            <p>
+              A melhor IPA que já provei! Recomendo a todos os amantes de
+              cerveja, sensacional!
+            </p>
+          </div>
           <h4>João Silva</h4>
         </div>
         <div className={Styles.cards}>
           <div className={Styles.img}>
             <img src="/avatar2.svg" />
           </div>
-          <p>
-            A Thor Thunder Stout é incrível! Encorpada e cheia de sabor, super
-            recomendo.
-          </p>
+          <div className={Styles.description}>
+            <p>
+              A Thor Thunder Stout é incrível! Encorpada e cheia de sabor, super
+              recomendo.
+            </p>
+          </div>
           <h4>Ana Paula</h4>
         </div>
         <div className={Styles.cards}>
           <div className={Styles.img}>
             <img src="/avatar2.svg" />
           </div>
-          <p>
-            Adorei a Freya Enchantment Wheat Beer. Leve e refrescante, nova
-            favorita.
-          </p>
+          <div className={Styles.description}>
+            <p>
+              Adorei a Freya Enchantment Wheat Beer. Leve e refrescante, nova
+              favorita.
+            </p>
+          </div>
           <h4>Mariana Lopes</h4>
         </div>
         <div className={Styles.cards}>
           <div className={Styles.img}>
             <img src="/avatar1.svg" />
           </div>
-          <p>
-            A Valhalla Lager é pura e refrescante, de sabor único é ótima para
-            qualquer ocasião.
-          </p>
+          <div className={Styles.description}>
+            <p>
+              A Valhalla Lager é pura e refrescante, de sabor único é ótima para
+              qualquer ocasião.
+            </p>
+          </div>
           <h4>Pedro Souza</h4>
         </div>
       </div>
